48c53f63013eced4437929f6a06d1357be33cb35
ayanda <64249187+ayandagatsha@users.noreply.github.com>
2021-07-21 05:02:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+//actors-styled html objects
+let box = document.getElementById("content");
+
+
+
+
+//action-behavior
+
+function myFunction() {
+    setTimeout(function(){ 
+   
+        box.style.backgroundColor="pink"
+     }, 7000);
+
+
+  }
+
+  myFunction();
